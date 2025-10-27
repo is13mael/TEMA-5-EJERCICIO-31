5f0e76aaeb26a068d8ba3b557f5650ae0cbbc61e
is13mael <ismaelparramoussaif@gmail.com>
2025-10-27 21:10:05 +0100

    second commit

--- src/App.java
+++ src/App.java
@@ -1,18 +1,18 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        try{
-        System.out.print("Introduzca la altura de la L: ");
-        int altura = Integer.parseInt(System.console().readLine());
-        for(int i = 0; i < altura; i++){
-            System.out.println("*");
-            
-        }
-        for(int j = 0; j < altura / 2 + 1; j++){
+        try {
+            System.out.print("Introduzca la altura de la L: ");
+            int altura = Integer.parseInt(System.console().readLine());
+            for (int i = 0; i < altura; i++) {
+                System.out.println("*");
+
+            }
+            for (int j = 0; j < altura / 2 + 1; j++) {
                 System.out.print("* ");
             }
             System.out.println();
-    }catch(NumberFormatException e){
-        System.out.println("");
+        } catch (NumberFormatException e) {
+            System.out.println("");
+        }
     }
-}
 }
